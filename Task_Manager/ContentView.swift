2e7e583cb0b2ac337fde9946c46206dbe33f4550
eyhdev <alaqsa335@gmail.com>
2024-01-07 14:47:49 +0300

    Initial Commit

---
+++ Task_Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Task_Manager
+//
+//  Created by eyh.mac on 7.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Task_Manager/Task_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagerApp.swift
+//  Task_Manager
+//
+//  Created by eyh.mac on 7.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
